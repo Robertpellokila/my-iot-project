205d309b78b8dc4e4684d47c5cdfa0af5833c0ec
Robertpellokila <robertpellokila09@gmail.com>
2026-09-23 23:43:24 +0800

    fix wrangler

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), cloudflare()],
+  plugins: [vue()],
 })
